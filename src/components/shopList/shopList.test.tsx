c18d67a9e2cf394f2319b1742642a38783e6d2b1
Ricardo Beaujon <rbeaujon77@gmail.com>
2023-04-22 12:31:26 +0300

    test: added shopList test
    this test has a total lines coverage of 73.68%, with  70% statements and 66.66% functions

---
+++ src/components/shopList/shopList.test.tsx
@@ -0,0 +1,39 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import { ShopList } from './shopList';
+import data from '../services/api/data.json'
+
+describe('ShopList', () => {
+  test('renders options for each shop', async () => {
+    render(<ShopList error={false} />);
+
+    await waitFor(() => {
+      const options = screen.getAllByRole('option');
+      expect(options.length).toBeGreaterThan(1);
+    });
+
+    const options = screen.getAllByRole('option');
+    expect(options.length).toBe(4);
+  });
+});
+
+
+test('select is render?', async () => {
+  
+  jest.spyOn(window, 'fetch').mockImplementation(() => {
+    return Promise.resolve({
+      json: () => Promise.resolve(data.shopsList),
+    } as Response);
+  });
+  
+  render(<ShopList error={false} />);
+  
+  const selectElement = screen.getByRole('combobox');
+  const options = screen.getAllByRole('option');
+  
+  expect(selectElement).toBeInTheDocument();
+  await waitFor(() => {
+    const options = screen.getAllByRole('option');
+    expect(options.length).toEqual(data.shopsList.length + 1);
+  });
+
+});
